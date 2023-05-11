638dee7401916703fed32d70afae93f37d7c6413
MarcosCoronel <coronel.mlc1@gmail.com>
2023-05-10 21:24:01 -0300

    browser router funcionando

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './css/index.css'
 import './Components/Header.jsx'
@@ -7,7 +8,9 @@ import './Components/Header.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>,
   
 )
